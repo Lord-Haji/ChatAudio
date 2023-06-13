d94a7988230de12ded1ba7ceb0c53e52bee147bb
Lord-Haji <lordhajisinbox@gmail.com>
2023-06-13 12:41:16 +0530

    Update example in main.py

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
-from langchain.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import Chroma
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.document_loaders import UnstructuredPDFLoader
 from langchain.llms import OpenAI
 from langchain.chains.question_answering import load_qa_chain
+from transcriber import audio_to_text
 
-# Replace book.pdf with any pdf of your choice
-loader = UnstructuredPDFLoader("book.pdf")
-pages = loader.load_and_split()
+
+
+file_path = "cancel_2.wav"
+audio_transcript = audio_to_text(file_path)
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+splitted_text = text_splitter.split_text(audio_transcript)
 embeddings = OpenAIEmbeddings()
-docsearch = Chroma.from_documents(pages, embeddings).as_retriever()
+audio_search = Chroma.from_texts(splitted_text, embeddings).as_retriever()
 
-# Choose any query of your choice
-query = "Who is Rich Dad?"
-docs = docsearch.get_relevant_documents(query)
+
+query = "What is name and DOB of customer?"
+docs = audio_search.get_relevant_documents(query)
+
 chain = load_qa_chain(OpenAI(temperature=0), chain_type="stuff")
 output = chain.run(input_documents=docs, question=query)
 print(output)
